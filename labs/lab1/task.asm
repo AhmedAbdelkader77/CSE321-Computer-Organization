de3a89e54ec1d1df79c2c9795a8f7d44a77282ba
Abd-Elkader <ahmedm735k@gmail.com>
2025-11-27 14:00:51 +0200

    upload labs

---
+++ labs/lab1/task.asm
@@ -0,0 +1,22 @@
+.data
+    name: .asciiz "Name: ahmed mohamed mohamed mohamed abd-elkader\n"
+    id : .asciiz "ID:   20812022100254\n"
+    course: .asciiz "Course: CSE321-Computer-Organization\n"
+
+.text
+main:
+    li $v0,4
+    la $a0,name
+    syscall
+
+     li $v0,4
+    la $a0,id
+    syscall
+    
+     li $v0,4
+    la $a0,course
+    syscall
+
+    li $v0,10
+    syscall
+
